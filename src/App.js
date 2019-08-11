e16f9031df616f479a382038c823f93dfa30c642
Mikhail Baranov <baranov@arrival.com>
2019-08-11 19:56:32 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Layout, Menu, Row, Col, Card} from 'antd';
+import LoginForm from './LoginForm';
+
+const {Header, Footer, Content} = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{minHeight: '100vh'}}>
+      <Layout>
+        <Header>
+          <Row type="flex" justify="space-between">
+            <Col span={6} style={{color: "white"}}>NoBody Inc</Col>
+            <Col span={3}>
+              <Menu
+                mode="horizontal"
+                defaultSelectedKeys={["en"]}
+                style={{lineHeight: '64px'}}>
+                <Menu.Item key="en">En</Menu.Item>
+                <Menu.Item key="ru">Ru</Menu.Item>
+              </Menu>
+            </Col>
+          </Row>
+        </Header>
+        <Content style={{padding: 36}}>
+          <Row>
+            <Col span={8} offset={8}>
+              <Card title="Login">
+                <LoginForm />
+              </Card>
+            </Col>
+          </Row>
+        </Content>
+        <Footer>
+          <Row>
+            <Col span={6}>&copy; 2019 Nobody Inc</Col>
+          </Row>
+        </Footer>
+      </Layout>
     </div>
   );
 }
