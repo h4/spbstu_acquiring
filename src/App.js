36a3e33e5b78f8e155a3d60b4ae708edc53225c3
Mikhail Baranov <baranov@arrival.com>
2019-10-05 11:11:12 +0300

    Improve login form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './App.css';
-import {Layout, Menu, Row, Col, Card} from 'antd';
-import LoginForm from './LoginForm';
+import React from "react";
+import "./App.css";
+import {Layout, Menu, Row, Col, Card} from "antd";
+import LoginForm from "./LoginForm";
 
 const {Header, Footer, Content} = Layout;
 
 function App() {
   return (
-    <div style={{minHeight: '100vh'}}>
+    <div style={{minHeight: "100vh"}}>
       <Layout>
         <Header>
           <Row type="flex" justify="space-between">
             <Col md={6} style={{color: "white"}}>NoBody Inc</Col>
-            <Col md={3}>
+            <Col md={5} lg={3}>
               <Menu
                 mode="horizontal"
                 defaultSelectedKeys={["en"]}
-                style={{lineHeight: '64px'}}>
+                style={{lineHeight: "64px"}}>
                 <Menu.Item key="en">En</Menu.Item>
                 <Menu.Item key="ru">Ru</Menu.Item>
               </Menu>
@@ -25,7 +25,7 @@ function App() {
         </Header>
         <Content style={{padding: 36}}>
           <Row>
-            <Col md={{ span: 8, offset: 8 }}>
+            <Col md={{ span: 16, offset: 4 }} lg={{ span: 8, offset: 8 }}>
               <Card title="Login">
                 <LoginForm />
               </Card>
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -12,25 +12,25 @@ class NormalLoginForm extends React.Component {
   };
 
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const {getFieldDecorator} = this.props.form;
     return (
       <Form onSubmit={this.handleSubmit} className="login-form">
         <Form.Item>
           {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input your username!' }],
+            rules: [{required: true, message: 'Please input your username!'}],
           })(
             <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
               placeholder="Username"
             />,
           )}
         </Form.Item>
         <Form.Item>
           {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
+            rules: [{required: true, message: 'Please input your Password!'}],
           })(
             <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
               type="password"
               placeholder="Password"
             />,
@@ -38,28 +38,32 @@ class NormalLoginForm extends React.Component {
         </Form.Item>
         <Form.Item>
           <Row type="flex" justify="space-between">
-            {getFieldDecorator('remember', {
-              valuePropName: 'checked',
-              initialValue: true,
-            })(<Checkbox>Remember me</Checkbox>)}
-            <a className="login-form-forgot" href="">
-              Forgot password
-            </a>
+            <div>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(<Checkbox>Remember me</Checkbox>)}
+              <Button
+                type="primary"
+                htmlType="submit"
+                block={true}
+                className="login-form-button">
+                Log in
+              </Button>
+            </div>
+
+            <div style={{lineHeight: 1.6, marginTop: 10}}>
+              <div><a className="login-form-forgot" href="/remember">Forgot password</a></div>
+              <div><a href="/register">Register</a></div>
+              <div><a href="http://lk.spbstu.ru/">Login with SPbPU ID</a></div>
+            </div>
           </Row>
-          <Button
-            type="primary"
-            htmlType="submit"
-            block={true}
-            className="login-form-button">
-            Log in
-          </Button>
-          Or <a href="">register now!</a>
         </Form.Item>
       </Form>
     );
   }
 }
 
-const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
+const WrappedNormalLoginForm = Form.create({name: 'normal_login'})(NormalLoginForm);
 
 export default WrappedNormalLoginForm;
